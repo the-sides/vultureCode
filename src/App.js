ef075d491ab3f381055bf856e93dc72d96f58ed9
Jacob Sides <jake@Jacobs-MBP.hsd1.tn.comcast.net>
2019-04-22 20:15:36 -0400

    panel constructor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './static/style.sass';
 import './App.css'
 import { Button} from 'react-materialize'
 import M from 'materialize-css'
 
-// 
-let crawler = () => {
-  console.log("boo")
+// let crawler = () => { console.log("crawler") }
+
+const newPanel = () => {
+  console.log("New panel");
+  // Create new 
+  let app = document.getElementById('App');
+  let panelDOM = document.createElement('div');
+  panelDOM.className = "panelReact"
+  app.appendChild(panelDOM);
+
+  ReactDOM.render(
+      <Panel title="StackOverflow: sorting arrays in javascript" 
+              content="function(){this is code}"/>,
+      panelDOM
+  )
 }
 
 function Search(){
@@ -17,7 +30,7 @@ function Search(){
         <label htmlFor="keywords"></label>
       </div>
       <Button id ="searchBtn" className="col s2 offset-s1" 
-          waves='green' onClick={crawler}>Scavenge</Button>
+          waves='green' onClick={newPanel}>Scavenge</Button>
     </div>
   )
 }
@@ -28,18 +41,9 @@ function Panel(props){
     <div className='phead'>
         {props.title}
     </div>
-    <pre>{`
-    // Imported via webscraper
-    function sort(a, b){
-      if(a < b){
-        doStuff();
-      }
-      else
-        giveUp();
-    }
-    WE CAN DO MORE!
-    `}</pre>
-  </div>)
+    <pre>{props.content}</pre>
+  </div>
+  )
 }
 
 class App extends Component {
@@ -48,9 +52,12 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div id="App">
           <Search/>
-          <Panel title="StackOverflow: sorting arrays in javascript" code="function(){this is code}"/>
+          <div className="panelReact">
+            <Panel title="StackOverflow: sorting arrays in javascript" 
+                  content="function(){this is code}"/>
+          </div>
       </div>
     );
   }
