a78d0ebff4daa44253ffe1627ada4bf2283815df
Jacob Sides <jake@Jacobs-MacBook-Pro.local>
2019-05-02 12:31:36 -0400

    paper finished, project abandoned, someone finish me plz

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './static/style.sass';
 import './App.css'
 import { Button} from 'react-materialize'
 import M from 'materialize-css'
+// For webscrapper
+const query = 'request'
 
 // let crawler = () => { console.log("crawler") }
+// query("google");
 
 const newPanel = () => {
   console.log("New panel");
@@ -15,6 +18,7 @@ const newPanel = () => {
   panelDOM.className = "panelReact"
   app.appendChild(panelDOM);
 
+  // But I should not be calling this often?
   ReactDOM.render(
       <Panel title="StackOverflow: sorting arrays in javascript" 
               content="function(){this is code}"/>,
@@ -41,7 +45,7 @@ function Panel(props){
     <div className='phead'>
         {props.title}
     </div>
-    <pre>{props.content}</pre>
+    <code>{props.content}</code>
   </div>
   )
 }
@@ -55,8 +59,30 @@ class App extends Component {
       <div id="App">
           <Search/>
           <div className="panelReact">
-            <Panel title="StackOverflow: sorting arrays in javascript" 
-                  content="function(){this is code}"/>
+            <Panel title="GeeksForGeeks: Sample Query Responce" 
+                  content="function func() { 
+  
+                    //Original string 
+                    var arr = [2, 5, 8, 1, 4] 
+                  
+                    //Sorting the array 
+                    document.write(arr.sort()); 
+                    document.write('<br>'); 
+                    document.write(arr); 
+                } 
+                func(); "/>
+            <Panel title="StackOverflow: Sorting an array of JavaScript objects by property" content="var sort_by = function(field, reverse, primer){
+            var key = primer ? 
+                function(x) {return primer(x[field])} : 
+                function(x) {return x[field]};
+
+            reverse = !reverse ? 1 : -1;
+
+            return function (a, b) {
+                return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+              } 
+            }"></Panel>
+            <Panel title="Using JavaScript's sort Method for Sorting Arrays of Numbers" content="myArray.sort((a, b) => a - b);"></Panel>
           </div>
       </div>
     );
---
+++ src/request.js
@@ -0,0 +1,6 @@
+// const rp = require('request-promise');
+function query(qs){
+    console.log("Run search for ", qs);
+}
+// export default query;
+module.exports = query;
