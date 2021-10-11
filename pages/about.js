ceb49ec8b9f1a64572b9290b7cc4042e267c2fdb
Jacob Sides <jsides@pyxl.com>
2021-10-10 20:23:46 -0700

    Replace CRA with Next TW example

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>About page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+	  hello
+	  </div>
+  )
+}
--- src/request.js
+++
@@ -1,6 +0,0 @@
-// const rp = require('request-promise');
-function query(qs){
-    console.log("Run search for ", qs);
-}
-// export default query;
-module.exports = query;
