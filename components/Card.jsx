17d8846608fe8b355709a094184f774f558c8690
Jacob Sides <jsides@pyxl.com>
2021-10-11 20:19:56 -0700

    Add button FE

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,20 @@
+import Add from "../svgs/add.svg";
+
 export default function Card(props) {
-	return (
-		<div className="bg-[#F77F4C] flex flex-col drop-shadow-sm">	
-			<a href="#" className="text-left text-xl pt-3 pl-3 text-[#FEF3EE]"> {props.title} </a>
-			<pre className="text-left bg-[#FCD0BE] text-black p-8 -translate-x-4 translate-y-4 flex-1 overflow-auto">{props.content}</pre>
-		</div>
-	)
+  return (
+    <div className="bg-[#F77F4C] flex flex-col drop-shadow-sm relative group children:duration-300">
+      <a href="#" className="text-left text-xl pt-3 pl-3 text-[#FEF3EE]">
+        {props.title}
+      </a>
+      {/* This represents the top of the code but nothing more */}
+      <div className="relative h-0 w-full overflow-visible opacity-0 group-hover:opacity-100 transition-opacity">
+        <button className="absolute top-8 right-2 z-10">
+          <Add className=""></Add>
+        </button>
+      </div>
+      <pre className="text-left bg-[#FCD0BE] text-black p-8 -translate-x-4 translate-y-4 flex-1 overflow-auto group-hover:-translate-x-10 transition-transform">
+        {props.content}
+      </pre>
+    </div>
+  );
 }
