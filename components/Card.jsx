f3b8d73cb5e0d43877709fa4c90fcecaa89acb96
Jacob Sides <jsides@pyxl.com>
2021-10-11 18:04:38 -0700

    .jsx ftw

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props) {
 	return (
-		<div class="bg-[#F77F4C] flex flex-col">	
-			<a className="text-left text-xl pt-3 pl-3 text-[#FEF3EE]"> {props.title} </a>
+		<div className="bg-[#F77F4C] flex flex-col drop-shadow-sm">	
+			<a href="#" className="text-left text-xl pt-3 pl-3 text-[#FEF3EE]"> {props.title} </a>
 			<pre className="text-left bg-[#FCD0BE] text-black p-8 -translate-x-4 translate-y-4 flex-1 overflow-auto">{props.content}</pre>
 		</div>
 	)
