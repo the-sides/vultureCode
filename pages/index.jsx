c2fd130b1fe8eee8982545c4c8e2605c940414c4
Jacob Sides <jsides@pyxl.com>
2021-10-11 18:51:59 -0700

    Larger screen for wide monitors

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,14 +20,15 @@ window.addEventListener(‘DOMContentLoaded’, run);`
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header></Header>
-			<main className="flex flex-col items-center justify-start py-10 w-full flex-1 px-20 text-center bg-[#722505] text-white relative">
+			<main className="flex flex-col items-center justify-start py-10 w-full flex-1 px-8 md:px-20 text-center bg-[#722505] text-white relative">
 				<BirdSvg className="scale-125 origin-top-left absolute top-0 left-0"/>
 				<div className="w-[800px] max-w-full mx-auto relative">
 					<p className="text-left">Search results for...</p>
-					<h1 className="mt-4 w-full text-center text-6xl font-bold">{query}</h1>
+					<h1 className="mt-4 w-full text-center text-3xl md:text-6xl font-bold">{query}</h1>
+					<span className="absolute top-[-2rem] right-[-3rem] text-[160px] md:text-[200px] leading-[100%] font-header font-bold opacity-25">&rdquo;</span>
 				</div>
 
-				<div className="mt-12 w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-12 max-w-screen-xl 3xl:max-w-screen-2xl">
+				<div className="mt-16 w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-12 max-w-screen-xl 3xl:max-w-screen-2xl">
 					{[sampleRes, sampleRes, sampleRes, sampleRes].map((res, ind) => 
 						<Card {...res} ></Card>
 					)}
